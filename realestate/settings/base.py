3623672ef5620db36c0b7e2797bd0fa3a9a04ce4
patrick mugambi <patrick@kali.kali>
2022-10-28 20:16:54 +0300

    create model user and custom user manager

--- realestate/settings/base.py
+++ realestate/settings/base.py
@@ -130,7 +130,7 @@ MEDIA_ROOT = BASE_DIR / "mediafiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
-
+AUTH_USER_MODEL = "users.User"
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 import logging
 import logging.config
