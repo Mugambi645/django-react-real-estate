035118bb11609808baa9e5afdd8b28786b2083c9
patrick mugambi <patrick@kali.kali>
2022-10-29 10:26:25 +0300

    feat: register,login, update user profile

---
+++ apps/profiles/exceptions.py
@@ -0,0 +1,8 @@
+from rest_framework.exceptions import APIException
+class ProfileNotFound(APIException):
+    status_code = 404
+    default_detail = "The requested profile does not exist!"
+
+class NotYourProfile(APIException):
+    status_code = 403
+    default_detail = "You can't create a profile that does not belong to you"
